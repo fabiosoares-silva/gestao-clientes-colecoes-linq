2474ccf3def644ca12fa676894d1570ea9f61a2b
fabiosoares-silva <fabiosores.soares@gmail.com>
2026-04-04 08:30:04 -0300

    feat: Adicionar modelos e repositorios de clientes

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using GestaoDeClientesColecoesLinq.Repositorios;
+
+using var arquivo = new FileStream("clientes.csv", FileMode.Open, FileAccess.Read);
+using var stream = new StreamReader(arquivo);
+
+var clientes = CadastroRepositorio.BuscarClientes(stream);
+CadastroRepositorio.ExibirClientes(clientes);
+
---
+++ Repositorios/CadastroRepositorio.cs
@@ -0,0 +1,38 @@
+﻿using GestaoDeClientesColecoesLinq.Modelos;
+
+namespace GestaoDeClientesColecoesLinq.Repositorios;
+
+public class CadastroRepositorio 
+{
+    public static IEnumerable<Cliente> BuscarClientes(StreamReader stream)
+    {
+
+        var linha = stream.ReadLine();
+
+        while (linha is not null)
+        {
+            var partes = linha.Split(';');
+            var cliente = new Cliente
+            {
+                Id = int.Parse(partes[0]),
+                Nome = partes[1],
+                Telefone = partes[2],
+                Email = partes[3]
+            };
+            yield return cliente;
+            linha = stream.ReadLine();
+        }
+    }
+
+    public static void ExibirClientes(IEnumerable<Cliente> clientes)
+    {
+        var contador = 1;
+        Console.WriteLine("\nClientes:");
+        foreach (var cliente in clientes)
+        {
+            Console.WriteLine($" - {cliente.Nome}");
+            contador++;
+            if (contador > 30) break;
+        }
+    }
+}
